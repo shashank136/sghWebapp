800d5b338202c34a84597bb361ea272e7732276b
Shashank <shashank136.sk@gmail.com>
2019-01-06 18:38:46 +0530

    refactoring the code

--- src/main/java/com/sghwebapp/sghWebapp/repositories/RentersRepositroy.java
+++
@@ -1,9 +0,0 @@
-package com.sghwebapp.sghWebapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sghwebapp.sghWebapp.model.Renters;
-
-public interface RentersRepositroy extends CrudRepository<Renters, Long>{
-
-}
--- src/main/java/com/sghwebapp/sghWebapp/repositories/WorkersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sghwebapp.sghWebapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sghwebapp.sghWebapp.model.Workers;
-
-public interface WorkersRepository extends CrudRepository<Workers, Long>{
-
-}
